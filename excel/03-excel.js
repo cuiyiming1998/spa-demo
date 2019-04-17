034793f1c39cb696bfa20d4faf1a133b447815ef
cuiyiming1998 <1269567664@qq.com>
2019-04-17 15:22:32 +0800

    highlight

--- excel/03-excel.js
+++ excel/03-excel.js
@@ -2,7 +2,7 @@
 $(function() {
   var e = $(".main");
   return new Handsontable(e.get(0),{
-    data: [["login_name", "登录名", "C(9)", "N", "Y", ""], ["password", "密码", "C(9)", "N", "N", ""], ["chinese_name", "中文名", "C(10)", "N", "N", ""], ["english_name", "英文名", "C(10)", "Y", "N", ""], ["gender", "性别", "C(1)", "Y", "N", ""], ["address", "家庭住址", "C(100)", "Y", "N", ""], ["mobile", "联系电话", "C(15)", "Y", "N", ""], ["comment", "备注", "C(100)", "Y", "N", ""]],
+    data: [["login_name", "登录名", "C(9)", "N", "Y", ""], ["password", "密码", "C(9)", "N", "N", ""], ["chinese_name", "中文名", "C(10)", "N", "N", ""], ["english_name", "英文名", "C(10)", "Y", "N", ""], ["sex", "性别", "C(1)", "Y", "N", ""], ["address", "家庭住址", "C(100)", "Y", "N", ""], ["phone_num", "联系电话", "C(15)", "Y", "N", ""]],
          contextMenu: !0,
          manualRowResize: !0,
          manualColumnResize: !0,
