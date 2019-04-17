61bc5ed9a6f7bcb27493de25bd0a14e2d456f40e
cuiyiming1998 <1269567664@qq.com>
2019-04-17 15:15:14 +0800

    echart

--- echart/main.js
+++ echart/main.js
@@ -2,35 +2,35 @@ $(function(){
     var xData=[],
         yData=[];
 
-        for(var i = 0; i <= 1; i += 0.1) {
-          xData.push(roundFractional(i, 1));
-          yData.push(roundFractional(h(i), 2));
-        }
-      
-        var myChart = echarts.init($('.main').get(0));
-      
-        var option = {
-          title: {
-            text: '二进熵函数曲线'
-          },
-          tooltip: {},
-          legend: {
-            data:['信息量']
-          },
-          xAxis: {
-            data: xData
-          },
-          yAxis: {},
-          series: [{
-            name: '信息量',
-            type: 'line',
-            smooth: 'true',
-            data: yData
-          }]
-        };
-      
-        myChart.setOption(option);
-      
+for(var i = 0; i <= 1; i += 0.1) {
+    xData.push(roundFractional(i, 1));
+    yData.push(roundFractional(h(i), 2));
+  }
+
+  var myChart = echarts.init($('.main').get(0));
+
+  var option = {
+    title: {
+      text: '二进熵函数曲线'
+    },
+    tooltip: {},
+    legend: {
+      data:['信息量']
+    },
+    xAxis: {
+      data: xData
+    },
+    yAxis: {},
+    series: [{
+      name: '信息量',
+      type: 'line',
+      smooth: 'true',
+      data: yData
+    }]
+  };
+
+  myChart.setOption(option);
+
 
     myChart.setOption(option);
 
